2576488989660b526234e1a9d84841e9c5113eef
fadiabji <fadi.abji@htomail.com>
2022-12-06 21:03:18 +0100

    fixing database

--- AlphaWebApp/Data/ApplicationDbContext.cs
+++ AlphaWebApp/Data/ApplicationDbContext.cs
@@ -1,24 +1,42 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using AlphaWebApp.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AlphaWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-       
-
         public DbSet<Article> Articles { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Subscription> Subscriptions { get; set; }
         public DbSet<SubscriptionType> SubscriptionTypes { get; set; }
         public DbSet<WeatherForecast> WeatherForecasts { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new applicationUserEntityConfiguration());
+
+        }
+    }
+
+    public class applicationUserEntityConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(255);
+            builder.Property(u => u.LastName).HasMaxLength(255);
+            builder.Property(u => u.DateOfBirth).HasMaxLength(255);
+        }
     }
 }
--- AlphaWebApp/Services/IUserService.cs
+++ AlphaWebApp/Services/IUserService.cs
@@ -1,4 +1,4 @@
-﻿using AlphaWebApp.Areas.Identity.Data;
+﻿using AlphaWebApp.Models;
 
 namespace AlphaWebApp.Services
 {
