977e2ab94784f71732ad8c345e5a24738001edfb
fadiabji <fadi.abji@htomail.com>
2022-12-12 13:56:21 +0100

    added authorization to add rols

---
+++ AlphaWebApp/Models/ViewModels/CreateRoleVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AlphaWebApp.Models.ViewModels
+{
+    public class CreateRoleVM
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
