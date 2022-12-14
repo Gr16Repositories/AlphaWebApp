edd0278805ff7dc2cb2ff40c58041a77e6407568
Anton Örtlund <anton.ortlund@gmail.com>
2022-12-14 11:33:03 +0100

    Added icons to categories Db

--- AlphaWebApp/Models/Seeds/SeedCategoryData.cs
+++ AlphaWebApp/Models/Seeds/SeedCategoryData.cs
@@ -18,42 +18,50 @@ namespace AlphaWebApp.Models.Seeds
                 context.Categories.AddRange(
                     new Category
                     {
-                        name = "Local"
+                        name = "Local",
+                        icon = "fa-solid fa-map-pin"
                     },
 
                     new Category
                     {
-                        name = "Sweden"
+                        name = "Sweden",
+                        icon = "fa-sharp fa-solid fa-map"
                     },
 
                     new Category
                     {
-                        name = "World"
+                        name = "World",
+                        icon = "fa-solid fa-earth-americas"
                     },
 
                     new Category
                     {
-                        name = "Weather"
+                        name = "Weather",
+                        icon = "fa-solid fa-sun"
                     },
 
                     new Category
                     {
-                        name = "Economy"
+                        name = "Economy",
+                        icon = "fa-solid fa-chart-simple"
                     },
 
                     new Category
                     {
-                        name = "Health"
+                        name = "Health",
+                        icon = "fa-solid fa-heart-pulse"
                     },
 
                     new Category
                     {
-                        name = "Sport"
+                        name = "Sport",
+                        icon = "fa-solid fa-medal"
                     },
 
                     new Category
                     {
-                        name = "FIFA World Cup 2022"
+                        name = "FIFA World Cup 2022",
+                        icon = "fa-regular fa-futbol"
                     }
                 );
                 context.SaveChanges();
