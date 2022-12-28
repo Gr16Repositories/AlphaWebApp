cde504ed8c19b4923cac44dcf472edac74c19564
fadiabji <fadi.abji@htomail.com>
2022-12-28 19:57:52 +0100

    Search on search bar is working

--- AlphaWebApp/Controllers/ArticlesController.cs
+++ AlphaWebApp/Controllers/ArticlesController.cs
@@ -378,5 +378,56 @@ namespace AlphaWebApp.Controllers
             }
         }
 
+
+        // how to implement search for articles in my websit
+        // how to make search based on subscrption type
+        // two method one search for users anothor for premium subscriber that they could serch from archived articles 
+        //
+        // 
+        //@app.route('/search', methods=['POST'])
+        //def search():
+        //  query = request.form['query']
+        //  # do something with the query
+        //  return "Search results for: " + query
+
+        //[HttpPost]
+        //// GET: Movies
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(_movieService.MoviesIndex());
+        //}
+
+        //[HttpGet]
+        //public async Task<IActionResult> Index(string? movieSearch)
+        //{
+        //    ViewData["getMovieDetailes"] = movieSearch;
+        //    var movieQuery = from m in _context.Movies.ToList()
+        //                     select m;
+        //    if (!String.IsNullOrEmpty(movieSearch))
+        //    {
+        //        movieQuery = movieQuery.ToList().Where(m => m.Title.Contains(movieSearch));
+        //    }
+        //    return View(movieQuery);
+        //}
+
+        //public IActionResult Search(string query)
+        //{
+        //    // perform the search
+        //    var results = SearchService.Search(query);
+        //    return View(results);
+        //}
+
+//        @model IEnumerable<SearchResult>
+
+//<ul>
+//  @foreach (var result in Model)
+//        {
+//    < li > @result.Title </ li >
+//  }
+//</ul>
+
+
+
+
     }
 }
--- AlphaWebApp/Controllers/HomeController.cs
+++ AlphaWebApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using AlphaWebApp.Models.ViewModels;
 using AlphaWebApp.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace AlphaWebApp.Controllers
@@ -15,13 +16,15 @@ namespace AlphaWebApp.Controllers
         private readonly IStorageService _storageService;
         private readonly ApplicationDbContext _db;
         private readonly IArticleService _articleService;
+        private readonly ISubscriptionService _subscriptionService;
 
 
         public HomeController(/*ILogger<HomeController> logger,*/
                                 IEmailService emailService,
                                 IStorageService storageService,
-                                ApplicationDbContext db, 
-                                IArticleService articleService
+                                ApplicationDbContext db,
+                                IArticleService articleService,
+                                ISubscriptionService subscriptionService
                              )
         {
             //_logger = logger;
@@ -29,28 +32,52 @@ namespace AlphaWebApp.Controllers
             _storageService = storageService;
             _db = db;
             _articleService = articleService;
+            _subscriptionService = subscriptionService;
         }
 
         public IActionResult Index()
         {
             return View(_articleService.GetAllArticles());
         }
-        
+
         public IActionResult Privacy()
         {
             return View();
         }
-     
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }  
-
-   
-      
+        }
 
         
+        public IActionResult Search()
+        {
+            return View(new Article());
+        }
+
+
+        [HttpPost]
+        public IActionResult Search(string query)
+        {
+            var articlesList = _db.Articles.ToList();
+            if (_subscriptionService.HasSubscription(User))
+            {
+                var articleQuery = articlesList.Where(a => a.Content.Contains(query));
+                return View(articleQuery);
+            }
+            else if(!String.IsNullOrEmpty(query))
+            {
+                var articleQuery = articlesList.Where(a => a.Content.Contains(query) && a.Archive == false);
+                return View(articleQuery);
+            }
+            else
+            {
+                return View(new List<Article>());
+            }
+        }
+
     }
 }
