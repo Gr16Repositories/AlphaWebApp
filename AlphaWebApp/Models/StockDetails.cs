22cbead026ef82ee67af9ff3dd2c0a3bfa58a420
swathiBagepalli <swathi.laharika@gmail.com>
2022-12-28 10:40:29 +0100

    Added Stocks API controller

---
+++ AlphaWebApp/Models/StockDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace AlphaWebApp.Models
+{
+    public class StockDetails
+    {
+        public string Name { get; set; }
+
+        public string Symbol { get; set; }
+
+        public decimal Close { get; set; }
+
+        [DisplayName("Previous Close")]
+        public decimal PrevClose { get; set; }
+
+        [DisplayName("Percentage Change")]
+        public decimal PercentChange { get; set; }
+    }
+}
---
+++ AlphaWebApp/Services/StockService.cs
@@ -0,0 +1,25 @@
+﻿using AlphaWebApp.Models;
+
+namespace AlphaWebApp.Services
+{
+    public class StockService : IStockService
+    {
+        private readonly HttpClient _httpClient;
+
+        public StockService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    
+        public StockDetails GetStockDetails(string region)
+        {
+            StockDetails result = new StockDetails();
+            var response = _httpClient.GetAsync("https://stockapinewsapp.azurewebsites.net/summary?region={region}").Result;
+            if(response.IsSuccessStatusCode)
+            {
+               result  = response.Content.ReadFromJsonAsync<StockDetails>().Result;
+            }
+            return result;
+        }
+    }
+}
