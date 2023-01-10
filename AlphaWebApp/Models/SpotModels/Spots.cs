9532530e82eed57f61541966f72e730d1f4d7d47
fadiabji <fadi.abji@htomail.com>
2023-01-10 15:48:19 +0100

    Inital logic to SpotPrice has added using depenency injection

---
+++ AlphaWebApp/Models/SpotModels/Spots.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+using System.Text.Json.Serialization;
+
+namespace AlphaWebApp.Models.SpotModels
+{
+   
+
+    public class TodaysSpotData
+    {
+        [JsonPropertyName("todaysSpotPrices")]
+        public List<SpotPriceHour> TodaysSpotPrices { get; set; }
+
+    }
+    public class SpotPriceHour
+    {
+        [JsonPropertyName("spotData")]
+        public List<AreaSpotsData> SpotData { get; set; }
+    }
+
+    public class AreaSpotsData
+    {
+        [JsonPropertyName("dateAndTime")]
+        public DateTime DateAndTime { get; set; }
+
+        [JsonPropertyName("areaName")]
+        public string AreaName { get; set;}
+
+        [JsonPropertyName("price")]
+        public string Price { get; set; }
+
+
+    }
+}
