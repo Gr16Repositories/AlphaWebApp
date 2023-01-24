2016d371ebf9ccda2c9995ad2a5c97ea94c51dd9
swathiBagepalli <swathi.laharika@gmail.com>
2023-01-24 14:16:24 +0100

    Added New Currency converter API

--- AlphaWebApp/Program.cs
+++ AlphaWebApp/Program.cs
@@ -75,7 +75,10 @@ builder.Services.AddScoped<IStorageService, StorageService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ISubscriptionTypeService, SubscriptionTypeService>();
 //builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
+
 builder.Services.AddScoped<IStockService, StockService>();
+builder.Services.AddScoped<ICurrencyExchangeService, CurrencyExchangeService>();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IWeatherForcastService,WeatherForcastService>();
 builder.Services.AddScoped<ISubscriptionService,SubscriptionService>();
@@ -107,14 +110,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
 
-//app.UseResponseCaching();
+
+app.UseResponseCaching();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -124,8 +122,6 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
-
 
 app.MapControllerRoute(
     name: "default",
---
+++ AlphaWebApp/ViewComponents/CurrencyExchangeViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using AlphaWebApp.Models;
+using AlphaWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaWebApp.ViewComponents
+{
+    public class CurrencyExchangeViewComponent : ViewComponent
+    {
+        private readonly ICurrencyExchangeService currencyExchangeService;
+
+        public CurrencyExchangeViewComponent(ICurrencyExchangeService currencyExchangeService)
+        {
+            this.currencyExchangeService = currencyExchangeService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string from, string to)
+        {
+            return View();
+        }
+    }
+}
